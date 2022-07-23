6ecdce20077e551c3e84916d60c60951c44abc32
vipul <vipul01.thakur@gmail.com>
2022-07-23 19:39:32 +0530

    Linked List

---
+++ src/com/insignia/LinkedList/ReverseLinkedListRecursive.java
@@ -0,0 +1,203 @@
+package com.insignia.LinkedList;
+
+import java.io.*;
+
+public class ReverseLinkedListRecursive {
+  public static class Node {
+    int data;
+    Node next;
+  }
+
+  public static class LinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    void addLast(int val) {
+      Node temp = new Node();
+      temp.data = val;
+      temp.next = null;
+
+      if (size == 0) {
+        head = tail = temp;
+      } else {
+        tail.next = temp;
+        tail = temp;
+      }
+
+      size++;
+    }
+
+    public int size() {
+      return size;
+    }
+
+    public void display() {
+      for (Node temp = head; temp != null; temp = temp.next) {
+        System.out.print(temp.data + " ");
+      }
+      System.out.println();
+    }
+
+    public void removeFirst() {
+      if (size == 0) {
+        System.out.println("List is empty");
+      } else if (size == 1) {
+        head = tail = null;
+        size = 0;
+      } else {
+        head = head.next;
+        size--;
+      }
+    }
+
+    public int getFirst() {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else {
+        return head.data;
+      }
+    }
+
+    public int getLast() {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else {
+        return tail.data;
+      }
+    }
+
+    public int getAt(int idx) {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else if (idx < 0 || idx >= size) {
+        System.out.println("Invalid arguments");
+        return -1;
+      } else {
+        Node temp = head;
+        for (int i = 0; i < idx; i++) {
+          temp = temp.next;
+        }
+        return temp.data;
+      }
+    }
+
+    public void addFirst(int val) {
+      Node temp = new Node();
+      temp.data = val;
+      temp.next = head;
+      head = temp;
+
+      if (size == 0) {
+        tail = temp;
+      }
+
+      size++;
+    }
+
+    public void addAt(int idx, int val) {
+      if (idx < 0 || idx > size) {
+        System.out.println("Invalid arguments");
+      } else if (idx == 0) {
+        addFirst(val);
+      } else if (idx == size) {
+        addLast(val);
+      } else {
+        Node node = new Node();
+        node.data = val;
+
+        Node temp = head;
+        for (int i = 0; i < idx - 1; i++) {
+          temp = temp.next;
+        }
+        node.next = temp.next;
+
+        temp.next = node;
+        size++;
+      }
+    }
+
+    public void removeLast() {
+      if (size == 0) {
+        System.out.println("List is empty");
+      } else if (size == 1) {
+        head = tail = null;
+        size = 0;
+      } else {
+        Node temp = head;
+        for (int i = 0; i < size - 2; i++) {
+          temp = temp.next;
+        }
+
+        tail = temp;
+        tail.next = null;
+        size--;
+      }
+    }
+
+    public void removeAt(int idx) {
+      if (idx < 0 || idx >= size) {
+        System.out.println("Invalid arguments");
+      } else if (idx == 0) {
+        removeFirst();
+      } else if (idx == size - 1) {
+        removeLast();
+      } else {
+        Node temp = head;
+        for (int i = 0; i < idx - 1; i++) {
+          temp = temp.next;
+        }
+
+        temp.next = temp.next.next;
+        size--;
+      }
+    }
+
+    private void displayReverseHelper(Node node, int floor) {
+      if (node == null) {
+        return;
+      }
+
+      displayReverseHelper(node.next, floor + 1);
+      if (floor > size / 2) {
+        int tempData = ltemp.data;
+        ltemp.data = node.data;
+        node.data=tempData;
+      }
+      System.out.print(ltemp.data+" ");
+      ltemp = ltemp.next;
+    }
+
+    Node ltemp = null;
+    public void displayReverse() {
+      ltemp = head;
+      
+      displayReverseHelper(head,0);
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n1 = Integer.parseInt(br.readLine());
+    LinkedList l1 = new LinkedList();
+    String[] values1 = br.readLine().split(" ");
+    for (int i = 0; i < n1; i++) {
+      int d = Integer.parseInt(values1[i]);
+      l1.addLast(d);
+    }
+
+    int a = Integer.parseInt(br.readLine());
+    int b = Integer.parseInt(br.readLine());
+
+    l1.display();
+    l1.displayReverse();
+    l1.addLast(a);
+    l1.addFirst(b);
+    l1.display();
+  }
+}
