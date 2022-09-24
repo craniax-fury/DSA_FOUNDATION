b96648372ff2ed5b3d7b55d1b8e0adc47368d8ae
vipul <vipul01.thakur@gmail.com>
2022-09-24 18:06:05 +0530

    dp

---
+++ src/com/insignia/dynamicProgramming/BuySellStocks/HighwayBillboard.java
@@ -0,0 +1,50 @@
+package com.insignia.dynamicProgramming.BuySellStocks;
+
+import java.util.Scanner;
+
+public class HighwayBillboard {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+
+        scn.nextInt();
+
+        int boards = scn.nextInt();
+
+        int[] boardAt = new int[boards + 1];
+        int[] boardProfit = new int[boards + 1];
+
+        for (int i = 1; i <= boards; i++) {
+            boardAt[i] = scn.nextInt();
+        }
+
+        for (int i = 1; i <= boards; i++) {
+            boardProfit[i] = scn.nextInt();
+        }
+
+        int[] incMax = new int[boards + 1];
+        int[] excMax = new int[boards + 1];
+
+        int boardGap = scn.nextInt();
+
+        for (int i = 1; i <= boards; i++) {
+
+            if (boardAt[i] - boardAt[i - 1] >= boardGap) {
+                incMax[i] += incMax[i - 1] + boardProfit[i];
+
+            } else if (excMax[i - 1] + boardProfit[i] > incMax[i - 1]) {
+                incMax[i] = excMax[i - 1] + boardProfit[i];
+
+            } else {
+                incMax[i] = incMax[i - 1];
+            }
+
+            if (excMax[i - 1] > incMax[i - 1]) {
+                excMax[i] = excMax[i - 1];
+            } else {
+                excMax[i] = incMax[i - 1];
+            }
+        }
+
+        System.out.println(incMax[boards] > excMax[boards] ? incMax[boards] : excMax[boards]);
+    }
+}
