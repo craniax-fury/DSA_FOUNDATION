769a79c897eaf791fc8e393b2a80dcb8d2d4c799
vipul <vipul01.thakur@gmail.com>
2022-11-04 19:23:37 +0530

    dp

--- src/com/insignia/dynamicProgrammingRecursive/ClimbStairsMinMoves.java
+++ src/com/insignia/dynamicProgrammingRecursive/ClimbStairsMinMoves.java
@@ -6,39 +6,70 @@ import java.io.InputStreamReader;
 
 public class ClimbStairsMinMoves {
 
-    private static int climb(int dest,int curr, int[] jumps,int[] soln) {
-        
-        int min = Integer.MAX_VALUE;
+    private static int climb(int dest, int curr, int[] jumps, int[] soln) {
 
-        if(dest>dest){
+        if (curr == dest) {
+            soln[dest] = 1;
+            return 1;
+        }
+
+        if (soln[curr] != 0) {
+            return soln[curr] + 1;
+        }
+
+        if (jumps[curr] == 0) {
             return 0;
         }
 
-        if(soln[dest]!=0){
-            return soln[dest];
-        }
+        for (int i = 1; i <= jumps[curr]; i++) {
+            if (i + curr <= dest) {
+                int move = climb(dest, curr + i, jumps, soln);
 
-        climb(dest+1, jumps, soln);
-
-        if(jumps[dest]!=0){
-            for(int i=1;i<=jumps[dest];i++){
-                int path = climb(dest+i, jumps, soln);
-                if(soln[dest]+path<min){
-                    min=path+soln[dest];
+                if (soln[curr] == 0) {
+                    soln[curr] = move;
+                } else if (move != 0 && move < soln[curr]) {
+                    soln[curr] = move;
                 }
             }
-        }else{
-            int path = climb(dest-1, jumps, soln);
-                if(soln[dest]+path<min){
-                    min=path+soln[dest];
+        }
+
+        if (curr != 0 && soln[curr] != 0) {
+            return soln[curr] + 1;
+        } else {
+            return soln[curr];
+        }
+
+    }
+
+    private static int climbTabu(int dest, int curr, int[] jumps, int[] soln) {
+
+        for (int i = dest; i >= 0; i--) {
+            if (i == dest) {
+                soln[i] = 1;
+                continue;
+            }
+
+            if (jumps[i] == 0) {
+                continue;
+            }
+
+            for (int j = 1; j <= jumps[i]; j++) {
+                if (j + i <= dest) {
+
+                    if(soln[i+j]!=0 && i+j!=dest && soln[i+j]+1<soln[i]){
+                        soln[i] = soln[i+j]+1;
+                    
+                    }else if(i+j==dest){
+                        soln[i] = soln[i+j];
+                    
+                    }else if (soln[i] == 0 && soln[i+j]!=0) {
+                        soln[i] = soln[i+j]+1;
+                    }
                 }
+            }
         }
 
-        if(min!=0 && min<soln[dest]){
-            soln[dest]+=min;
-        }
-
-        return soln[dest];
+        return soln[0];
 
     }
 
@@ -46,17 +77,26 @@ public class ClimbStairsMinMoves {
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             int n = Integer.parseInt(reader.readLine());
-            int[] jumps = new int[n+1];
+            int[] jumps = new int[n + 1];
 
-            for (int i = 1; i <= n; i++) {
+            for (int i = 0; i < n; i++) {
                 jumps[i] = Integer.parseInt(reader.readLine());
             }
 
             int[] soln = new int[n + 1];
-            soln[n] = 0;
 
-            System.out.println(climb(n, jumps, soln));
+            // System.out.println(climb(n, 0, jumps, soln));
 
+            // display(soln);
+
+            System.out.println(climbTabu(n, 0, jumps, soln));
+
+        }
+    }
+
+    private static void display(int[] soln) {
+        for (int i = 0; i < soln.length; i++) {
+            System.out.print(soln[i] + " ");
         }
     }
 }
--- src/com/insignia/dynamicProgrammingRecursive/ClimbStairsVarJumps.java
+++ src/com/insignia/dynamicProgrammingRecursive/ClimbStairsVarJumps.java
@@ -13,43 +13,88 @@ public class ClimbStairsVarJumps {
             return 0;
         }
 
-        if (soln[curr] !=null) {
+        if (soln[curr] != null) {
             return soln[curr];
         }
 
-        while (curr != 0) {
+        while (curr != -1) {
             for (int j = 1; j <= jumps[curr]; j++) {
                 int move = climb(dest, curr + j, jumps, soln);
-                if(move!=-1){
-                    soln[curr]=move+1;
+                if (move != -1) {
+                    if (soln[curr] == null) {
+                        soln[curr] = move==0?1:move;
+                    } else {
+                        soln[curr] +=  move;
+                    }
+
                 }
             }
 
-            if(soln[curr]==null){
+            if (soln[curr] == null) {
                 soln[curr] = -1;
             }
             curr--;
         }
 
-        return soln[1];
+        return soln[0];
 
     }
+
+    private static int climbR(int dest, int[] dp,int curr,int[] jumps){
+        if(dp[curr]!=0){
+            return dp[curr];
+        }
+
+        if(curr==dest){
+            dp[curr]=1;
+            return 1;
+        }
+
+        if(jumps[curr]==0){
+            dp[curr]=0;
+            return 0;
+        }
+
+        for(int jump=1;jump<=jumps[curr];jump++){
+            if(jump+curr<=dest){
+                dp[curr]+= climbR(dest, dp, curr+jump, jumps);
+            }
+        }
+
+        return dp[curr];
+    } 
+    
+    private static int climbTab(int dest, int[] dp,int curr,int[] jumps){
+        
+        for(int i=dest;i>=0;i--){
+            
+            if(jumps[i]==0){
+                continue;
+            }
+    
+            for(int jump=1;jump<=jumps[i];jump++){
+                if(jump+i<=dest){
+                    dp[i]+= dp[i+jump];
+                }
+            }   
+        }
+
+        return dp[0];  
+    } 
 
     public static void main(String[] args) throws IOException {
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             int n = Integer.parseInt(reader.readLine());
-            int[] jumps = new int[n];
+            int[] jumps = new int[n+1];
 
             for (int i = 0; i < n; i++) {
                 jumps[i] = Integer.parseInt(reader.readLine());
             }
 
-            Integer[] soln = new Integer[n + 1];
-            Arrays.fill(soln, null);
-            soln[n] = 0;
-
-            System.out.println(climb(n, n - 1, jumps, soln));
+            int[] dp = new int[n + 1];
+dp[n]=1;
+            System.out.println(climbTab(n,dp,n , jumps));
 
         }
     }
